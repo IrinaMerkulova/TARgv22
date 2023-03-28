1149c30a29b7ce840b186598b54dc9dc743be9e4
Olga Petrova <opilane@tthk.ee>
2023-03-28 14:11:06 +0300

    vanuse summa

--- laused.sql
+++ laused.sql
@@ -146,7 +146,7 @@ select top 50 percent * from Person
 select * from Person order by cast(Age as int)
 select * from Person order by Age
 
---?
+--vanuse summa
 select sum(cast(Age as int)) from Person
 
 --- kuvab kõige nooremat isikut
