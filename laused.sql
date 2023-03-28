bab33283bf7c25a99b2650ad2f578800c6850280
MariaJuliaJarv <opilane@tthk.ee>
2023-03-28 13:16:13 +0300

    Andmed sisestatud tabelisse Gender, ja foreign key lisamine tabelis person

--- laused.sql
+++ laused.sql
@@ -24,8 +24,9 @@ insert into Gender (Id, Gender)
 values (1, 'Female')
 insert into Gender (Id, Gender)
 values (2, 'Male')
+select * from Gender
 
---- ?
+--- foreign key lisamine tabelisse Person
 alter table Person add constraint tblPerson_GenderId_FK
 foreign key (GenderId) references Gender(Id)
 
