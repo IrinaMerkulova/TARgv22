77894d4c4ea7982a8b3a0a22624e21dfef88af52
Vladimir_DudakovTarGV22 <opilane@tthk.ee>
2023-03-28 13:27:38 +0300

    Kahe tabelite ühendamine (Foreign Key) kaudu

--- laused.sql
+++ laused.sql
@@ -25,7 +25,7 @@ values (1, 'Female');
 insert into Gender (Id, Gender)
 values (2, 'Male')
 
---- ?
+--- tabeli muutmine piirangu lisamine - foreign key
 alter table Person add constraint tblPerson_GenderId_FK
 foreign key (GenderId) references Gender(Id)
 
@@ -54,12 +54,13 @@ drop constraint tblPerson_GenderId_FK
 
 -- sisestame väärtuse tabelisse
 insert into Gender (Id, Gender)
-values (3, 'Unknown')
+values (3, 'Unknown');
+Select * from Gender;
 -- lisame võõrvõtme uuesti
 alter table Person
 add constraint DF_Person_GenderId
 default 3 for GenderId
-
+Select * from Gender;
 
 ---- 2 tund
 
