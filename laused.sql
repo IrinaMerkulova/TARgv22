806630e057b6f95b55e28a8077b5c4479f0ef656
Olga Petrova <opilane@tthk.ee>
2023-03-28 14:08:10 +0300

    näitab vanus

--- laused.sql
+++ laused.sql
@@ -142,7 +142,7 @@ select top 3 Age, Name from Person
 
 --- näitab esimesed 50% tabelis
 select top 50 percent * from Person
---?
+--näitab vanus
 select * from Person order by cast(Age as int)
 select * from Person order by Age
 
