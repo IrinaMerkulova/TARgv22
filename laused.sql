d96b0f61193ca24c109f2c49172845434dd45499
RainonKaska <rainonkaskazxc@gmail.com>
2023-03-28 13:01:00 +0300

    3 sql laused tehtud

--- laused.sql
+++ laused.sql
@@ -25,7 +25,7 @@ values (1, 'Female')
 insert into Gender (Id, Gender)
 values (2, 'Male')
 
---- ?
+--- FK lisamine Person tabelis
 alter table Person add constraint tblPerson_GenderId_FK
 foreign key (GenderId) references Gender(Id)
 
