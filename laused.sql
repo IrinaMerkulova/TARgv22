6aa1a9554468010bfe5b5aaead02842d37dd9fe3
RainonKaska <rainonkaskazxc@gmail.com>
2023-03-28 13:26:24 +0300

    viimased laused ja tabelite sidumine tehtud

--- laused.sql
+++ laused.sql
@@ -25,7 +25,7 @@ values (1, 'Female')
 insert into Gender (Id, Gender)
 values (2, 'Male')
 
---- FK lisamine Person tabelis
+--- tabeli muutmine piirangi lisamine - foreign key
 alter table Person add constraint tblPerson_GenderId_FK
 foreign key (GenderId) references Gender(Id)
 
